58d9730dcb0e8d3085a55169dcb0604b0f2867b3
Дмитрий Константинович Холостов <dkh@ro.ru>
2022-12-04 20:29:21 +0300

    crunch for new elements

--- crutch_translate.js
+++ crutch_translate.js
@@ -1,11 +1,18 @@
 // https://github.com/dkh-gh/crutch_translate.js
 
-document.addEventListener('DOMContentLoaded', crutch_translate);
+window.addEventListener('load', crutch_translate);
+
+var crunch_translate_list = [];
 
 function crutch_translate() {
 	elems = document.body.querySelectorAll('*');
 	for(let i = 0; i < elems.length; i++) {
+		crunch_translate_list.push(elems[i].innerHTML);
 		if(crutch_translate_dict[elems[i].innerHTML] !== undefined)
 			elems[i].innerHTML = crutch_translate_dict[elems[i].innerHTML];
 	}
 }
+
+document.addEventListener('click', function() {
+	window.setTimeout(crutch_translate, 10);
+});
